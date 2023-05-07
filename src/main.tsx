17c183b2be0ed3d17ed51b5488ccea5f55cab7a5
yxyHeart <2541455671@qq.com>
2023-05-07 13:57:47 +0800

    Update main.tsx

--- src/main.tsx
+++ src/main.tsx
@@ -6,8 +6,8 @@ import { createRoot } from 'react-dom/client';
 import App from './App';
 import BaseStyles from './styles/BaseStyles';
 
-document.title = import.meta.env.VITE_APP_TITLE;
-
+// document.title = import.meta.env.VITE_APP_TITLE;
+document.title = "yxy's Blog"
 const rootEl = document.getElementById('root')!;
 const root = createRoot(rootEl);
 
